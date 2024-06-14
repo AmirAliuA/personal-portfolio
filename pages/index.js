16e6b651f1e96a0ee2360f0777947ed121b91928
Amir Aliu <aaliu272@gmail.com>
2024-06-14 11:26:57 +0200

    show projects on main page based on their creation date

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,10 @@ export default function Home({ allPosts, allProjects, post }) {
             Projects
           </h2>
           <div className="mt-4 grid grid-cols-2 md:grid-cols-3 gap-4">
-            {allProjects?.map((project) => (
+            {
+              allProjects
+              .sort((a, b) => new Date(b.date) - new Date(a.date))
+              .map((project) => (
               <div className="min-w-[120px] flex-col px-4 py-2 border border-gray-200 dark:border-gray-700/70 rounded-lg hover:bg-gray-100 dark:bg-gray-800/70 dark:hover:bg-gray-800 dark:hover:border-gray-700 dark:shadow-[0_0_8px_rgba(0,0,0,0.8)] shadow-[0_0_8px_rgba(0,0,0,0.06)] flex relative ">
                 {project?.ios ? (
                   <Link
